c4d3aecab61ea8cfff2da08528fce781b0cbc16e
creativeinnov <mehtalaxmi1234@gmail.com>
2025-09-27 00:34:28 +0530

    Git Deploy Updated

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/WeatherProject/', // Match your GitHub Pages URL path
+  base: '/WeatherProject', // Match your GitHub Pages URL path
   plugins: [react()],
 });
